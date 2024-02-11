31a06a1c5b76a75e443667643e7f17991915ef6b
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-11 13:44:37 -0800

    Task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,29 @@
+$().ready(function () {
+  const AmenitiesChecked = [];
+
+  function truncate(str, n){
+    return (str.length > n) ? str.slice(0, n-1) + '\u2026' : str;
+  };
+
+  $('.amenities input[type="checkbox"]').change(function () {
+    const IdAmenity = $(this).data('id');
+    const NameAmenity = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      AmenitiesChecked.push({ id: IdAmenity, name: NameAmenity });
+    } else {
+      for(var i = 0; i < AmenitiesChecked.length; i++) {
+        if(AmenitiesChecked[i].id == IdAmenity) {
+          AmenitiesChecked.splice(i, 1);
+          break;
+        }
+      }
+    }
+
+    console.log(AmenitiesChecked);
+    const AmenitiesList = AmenitiesChecked.map(function (amenity) {
+      return amenity.name;
+    }).join(', ');
+    $('.amenities h4').text(truncate(AmenitiesList, 40));
+  });
+});
